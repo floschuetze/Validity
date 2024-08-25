2fd92a9309789dc0039903dcf74cfeba6913ad0d
Florian Schuetze <florian.schuetze@uni-hamburg.de>
2024-08-25 18:35:35 +0200

    neu

--- R/Validity.R
+++ R/Validity.R
@@ -1,103 +1,222 @@
-validity <- function(X, y, r) {
-  X <- as.matrix(X)
-  y <- as.matrix(y)
-  N <- 1e3
+validity <- function(y, X, reg, v = 0, s = 0,l=0,r=0) {
+  
+  #  Test for the validity of a regression model.
+  # ---
+  # Input:
+  # y (n x 1) - dependent variable
+  # X (n x m) - regressors
+  # reg (@)   - regression function delifering fitted values
+  # v (1 x 1) - version of the test
+  # v == 0: Frahm-Sch?tze
+  # v == 1: Stute
+  # s (1 x 1) - test statistic (only relevant if v == 1)
+  # s == 0: Cram?r-von-Mises
+  # s == 1: Kolmogorov-Smirnov
+  # The choice of s does not matter for v = 0.
+  # l (1 x 1) - weight on the left of [0,1]
+  # r (1 x 1) - weight on the right of [0,1]
+  # The choice of l and r does not matter for v = 1.
+  # ---
+  # Output:
+  # p (1 x 1) - p-value
+  # t (1 x 1) - test statistic
+  #Florian Sch?tze 08/2024 based on Gabriel Frahm 02.06.2024
+  
+  if (missing(v)) v <- 0
+  if (missing(s)) s <- 0
+  if (missing(l)) l <- 0
+  if (missing(r)) r <- 0
+  N <- 1000
+  X<-as.matrix(X)
   n <- nrow(X)
   m <- ncol(X)
-
-  if (r == 0) {
-    r <- max(floor(log(n)), 2)
-  }
-
-  D <- cbind(1, X)
-  p <- solve(t(D) %*% D) %*% t(D) %*% y
-  alpha <- p[1]
-  beta <- p[-1]
-  f <- alpha + X %*% beta
-  Z <- cbind(X, y, f, y - f)
-  Z <- Z[order(Z[, m + 2]), ]
-
-  T2 <- numeric(r)
-  for (j in 1:r) {
-    l <- ceiling((j - 1) * n / r) + 1
-    u <- ceiling(j * n / r)
-    e <- Z[l:u, m + 3]
-    k <- length(e)
-    T2[j] <- sum(e)^2 / k
-  }
-  T <- sum(T2)
-
-  theta <- valtheta(X, y, alpha, beta, r)
-  X_sum <- rowSums(theta^2)
-
-  if (T > 0) {
-    P <- 1 - sum(X_sum <= T) / N
-  } else {
-    P <- 1
-  }
-
-  return(list(P = P, T = T, alpha = alpha, beta = beta))
-}
-
-valtheta <- function(X, y, alpha, beta, r) {
-  N <- 1e3
-  n <- nrow(X)
-  m <- ncol(X)
-  f <- alpha + X %*% beta
-  E <- matrix(sample(y - f, n * N, replace = TRUE), nrow = n, ncol = N)
-  Y <- alpha + X %*% matrix(rep(beta, N), ncol = N) + E
-  D <- cbind(1, X)
-  P <- solve(t(D) %*% D) %*% t(D) %*% Y
-
-  theta <- matrix(NA, nrow = N, ncol = r)
+  
+  f <- reg(y, X)
+  Data <- (y - f) - mean(y - f)
+  
+  E <- matrix(sample(Data, n * N, replace = TRUE), nrow = n, ncol = N)
+  
+  Y <- f + E
+  #Y <- readMat("Y.mat")$Y
+  
+  F <- matrix(0, nrow = n, ncol = N)
   for (i in 1:N) {
-    a <- P[1, i]
-    b <- P[-1, i]
-    F <- a + X %*% b
-    Z <- cbind(X, Y[, i], F, Y[, i] - F)
-    Z <- Z[order(Z[, m + 2]), ]
-    for (j in 1:r) {
-      l <- ceiling((j - 1) * n / r) + 1
-      u <- ceiling(j * n / r)
-      e <- Z[l:u, m + 3]
-      k <- length(e)
-      theta[i, j] <- sqrt(k) * mean(e)
-    }
+    F[, i] <- reg(Y[, i], X)
   }
-
-  return(theta)
+  F <- apply(Y, 2, function(y) reg(y, X))
+  # install.packages("R.matlab")
+  # library(R.matlab)
+  #F <- readMat("F.mat")$F
+  Z1 <- matrix(NA, nrow = n, ncol = m * n) #vorversion f?r z (repmat(X,1,n);)
+  for (i in 1:n) {
+    Z1[, (m * i - 1):(m * i)] <- X
+  }
+  
+  
+  
+  # Repeat the row 7 times
+  repeated_rows <- matrix(rep(X, n), nrow = n, byrow = TRUE)
+  
+  interleaved <- as.vector(t(X))
+  interleaved <- interleaved[order(rep(1:nrow(X), each=m), rep(1:m, times=n))]
+  Z2 <- matrix(rep(interleaved, n), nrow = n, byrow = TRUE)
+  A<-Z1-Z2
+  
+  
+  
+  
+  O<-matrix(0,nrow=n,ncol=m)
+  C<-matrix(0,nrow=0,ncol=m)
+  
+  for (ff in seq(1,ncol(A),m)){
+    for (j in 1:m){
+      O[,j]<-A[,(ff-1+j)]}
+    
+    C<-rbind(C,O)
+  }
+  B<-C*-1
+  col_vector <- 1:n
+  
+  # Replicate the column vector across columns
+  k <- matrix(rep(col_vector, each = n), nrow = n, byrow = TRUE)
+  
+  # Compute the Beta PDF values
+  beta_pdf_values <- dbeta(((1:n) / n), shape1 = r + 1, shape2 = l + 1)
+  
+  # Replicate the vector horizontally
+  w <- matrix(rep(beta_pdf_values, each = n), nrow = n, byrow = TRUE)
+  K <- array(rep(1:n, n), dim = c(n, n, N))
+  W <- array(rep(w, each = N), dim = c(dim(w), N))
+  
+  if (v == 0){
+    C<-matrix(0,nrow=0,ncol=1)
+    
+    for (i in 1:n){
+      o<-seq(i,n*n,n)
+      o<-as.matrix(o)
+      C<-rbind(C,o)
+    }
+    if (m>1){
+      kkkk<-matrix(sqrt(rowSums(B[C, ]^2)), nrow = n,ncol=n, byrow = TRUE)
+    }else{
+      kkkk<-matrix(sqrt(B[C,1 ]^2), nrow = n,ncol=n, byrow = TRUE)
+      
+    }
+    I<-matrix(0,nrow=n,ncol=n)
+    for (i in 1:ncol(kkkk)){
+      I[,i]<-order(kkkk[,i])
+    }
+    
+    d <- y-f
+    e<-matrix(0,nrow=n,ncol=n)
+    for (i in 1:n){
+      e[,i] = d[I[,i]]
+    }
+    g<-matrix(0,nrow=n,ncol=n)
+    for (i in 1:n){
+      g[,i] = cumsum(e[,i])
+    }
+    
+    # Compute the mean along the second dimension (columns)
+    t <- mean(apply((w * g * g / k), 2, mean))
+    
+    # Reshape to a 3D array (100x1x1000)
+    R <- array((Y - F), dim = c(100, 1, 1000))
+    #####hier#####
+    
+    #D ist gesampelt, achtung!
+    E <- array(R[matrix(I, n^2, 1),,], dim = c(n, n, N))
+    
+    G <- apply(E, c(2, 3), cumsum)
+    
+    
+    # Compute the mean across the second dimension (rows in MATLAB)
+    T <- apply(apply(((W * G * G) / K), c(1, 3), mean),2,mean)
+  } else if (v == 1){
+    I <- array(rowSums(B <= 0) == m, dim = c(n, n))
+    
+    # e is a matrix where rows are repeated 'n' times
+    e <- matrix(rep(y - f, n), nrow = length(y), ncol = n)
+    # Set elements of e to 0 where I is TRUE
+    e[I] <- 0
+    
+    # E is a 3D array created by replicating Y - F across dimensions
+    
+    
+    # Convert the 2D matrix into a 3D array with dimensions (100, 1, 1000)
+    Ex <- array((Y - F), dim = c(nrow(Y - F), 1, ncol(Y - F)))
+    
+    E <- aperm(array(rep(Ex, each = n), dim = c(dim(Ex)[1], n, dim(Ex)[3])), c(2, 1, 3))
+    E[array(I, dim = c(nrow(I), ncol(I), N))]=0
+    
+    
+    if (s == 0) {
+      t <- sum(colSums(e)^2) / n^2
+      
+      T <- colSums(apply(E, c(3), function(x) colSums(x))^2, dims = 1)/n^2
+      
+      
+      
+    } else if (s == 1) {
+      t <- max(abs(colSums(e)))
+      T<-apply(abs(apply(E, c(3), function(x) colSums(x))),2,max)
+    } else {
+      stop("Test statistic not properly specified!")
+    }
+  } else {
+    
+    print("Error:Test version not properly specified!")
+  }
+  # Compute the mean along the first dimension (rows)
+  
+  # Mean along the first dimension (rows)
+  # library(pracma)
+  # h <- repmat(matrix(1:n, n, 1), 1, n)
+  # t <- mean(apply(g * g / h, 2, mean))
+  # H <- array(rep(1:n, times = n * N), dim = c(n, n, N))
+  # T <- apply(G * G / H, c(2, 3), mean) 
+  # T <- apply(T, 2, mean)  
+  
+  
+  if (t <= .Machine$double.eps){
+    t = 0
+    p = 1
+  }else{
+    p <- 1 - sum(T <= t) / N}
+  return(list(P = p, T = T, t = t))
+  
 }
 
+reg<-function(y, X) {
+  d<-data.frame(X,y1=y)
+  model <- lm(y1 ~ ., data = d)
+  return(model$fitted.values)
+}
 
-constant <- function(c, n) {
+onlyconstant <- function(c,n) {
   x <- rnorm(n, mean=0, sd=1)
   y <- rnorm(n, mean=0, sd=1)
   y[which(x>0)]<- y[which(x>0)]+c
   y[which(x<=0)]<- y[which(x<=0)]-c
-  z<-cbind(x,y)
-  return(z)
+  p<-validity(x,y,reg,0,0,0,0)
+  return(p)
 }
 
-quadratic <- function(c, n) {
+quadratic <- function(c,n) {
+  
   x <- rnorm(n, mean=0, sd=1)
-  y<-rnorm(n, mean=0, sd=1)-1+x+c*((x^2)-1)
-  z<-cbind(x,y)
-  return(z)
+  y<-(-1)+x+c*((x^2)-1)+rnorm(n, mean=0, sd=1)
+  p<-validity(x,y,reg,0,0,0,0)
+  return(p)
 }
 
-cobbdouglas <- function(c, n) {
+cobbdouglas <- function(c,n) {
+  
   require(MASS)
   sigma<-rbind(c(1,0.5), c(0.5,1))
-  mu<-c(0, 0)
+  mu<-c(0, 0) 
   LK<-as.matrix(mvrnorm(n=n, mu=mu, Sigma=sigma))
-  y<- rnorm(n, mean=0, sd=1)+0.25*LK[,1]+0.75*LK[,2]+c*LK[,1]*LK[,2]
-  d <- data.frame(Y=y,X1=LK[,1],X2=LK[,2])
-  return(d)
-}
-
-stute1997 <- function(a, sigma) {
-  x <- runif(400, min = 0, max = 1)
-  y<-  5*x+a*x^2+rnorm(400, mean=0, sd=sqrt(sigma))
-  d <- data.frame(Y=y,X1=x)
-  return(d)
+  y<-0.25*LK[,1]+0.75*LK[,2]+c*LK[,1]*LK[,2]+rnorm(n, mean=0, sd=1)
+  p<-validity(LK,y,reg,0,0,0,0)
+  return(p)
 }
