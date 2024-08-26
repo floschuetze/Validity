117ce8de9d23690af3fdf0ad4366a308f8aecd86
Florian Schuetze <florian.schuetze@uni-hamburg.de>
2024-08-26 17:18:38 +0200

    details hinzugefügt

--- R/Validity.R
+++ R/Validity.R
@@ -22,7 +22,7 @@ validity <- function(y, X, reg, v = 0, s = 0,l=0,r=0) {
   #' y <- x^3-x^2
   #' validity(y,x)
   #' @references 
-  #'Frahm, G.,2023, A Test for the Validity of Regression Models. Available on SSRN: https://ssrn.com/abstract=4610329
+  #'Frahm, G., 2023, A Test for the Validity of Regression Models. Available on SSRN: https://ssrn.com/abstract=4610329
   #'
   #'Stute,W.,1997, Nonparametric model checks for regression, The Annals of Statistics 25, pp. 613 to 641.
   #' @author Florian Schuetze 26.08.2024
@@ -69,24 +69,20 @@ validity <- function(y, X, reg, v = 0, s = 0,l=0,r=0) {
   E <- matrix(sample(Data, n * N, replace = TRUE), nrow = n, ncol = N)
   
   Y <- f + E
-  #Y <- readMat("Y.mat")$Y
-  
+
   F <- matrix(0, nrow = n, ncol = N)
   for (i in 1:N) {
     F[, i] <- reg(Y[, i], X)
   }
   F <- apply(Y, 2, function(y) reg(y, X))
-  # install.packages("R.matlab")
-  # library(R.matlab)
-  #F <- readMat("F.mat")$F
-  Z1 <- matrix(NA, nrow = n, ncol = m * n) #vorversion f?r z (repmat(X,1,n);)
+
+  Z1 <- matrix(NA, nrow = n, ncol = m * n) 
   for (i in 1:n) {
     Z1[, (m * i - 1):(m * i)] <- X
   }
   
   
   
-  # Repeat the row 7 times
   repeated_rows <- matrix(rep(X, n), nrow = n, byrow = TRUE)
   
   interleaved <- as.vector(t(X))
@@ -109,13 +105,10 @@ validity <- function(y, X, reg, v = 0, s = 0,l=0,r=0) {
   B<-C*-1
   col_vector <- 1:n
   
-  # Replicate the column vector across columns
   k <- matrix(rep(col_vector, each = n), nrow = n, byrow = TRUE)
   
-  # Compute the Beta PDF values
   beta_pdf_values <- dbeta(((1:n) / n), shape1 = r + 1, shape2 = l + 1)
   
-  # Replicate the vector horizontally
   w <- matrix(rep(beta_pdf_values, each = n), nrow = n, byrow = TRUE)
   K <- array(rep(1:n, n), dim = c(n, n, N))
   W <- array(rep(w, each = N), dim = c(dim(w), N))
@@ -149,33 +142,22 @@ validity <- function(y, X, reg, v = 0, s = 0,l=0,r=0) {
       g[,i] = cumsum(e[,i])
     }
     
-    # Compute the mean along the second dimension (columns)
     t <- mean(apply((w * g * g / k), 2, mean))
     
-    # Reshape to a 3D array (100x1x1000)
     R <- array((Y - F), dim = c(n, 1, N))
-    #####hier#####
-    
-    #D ist gesampelt, achtung!
+
     E <- array(R[matrix(I, n^2, 1),,], dim = c(n, n, N))
     
     G <- apply(E, c(2, 3), cumsum)
     
     
-    # Compute the mean across the second dimension (rows in MATLAB)
     T <- apply(apply(((W * G * G) / K), c(1, 3), mean),2,mean)
   } else if (v == 1){
     I <- array(rowSums(B <= 0) == m, dim = c(n, n))
     
-    # e is a matrix where rows are repeated 'n' times
     e <- matrix(rep(y - f, n), nrow = length(y), ncol = n)
-    # Set elements of e to 0 where I is TRUE
     e[I] <- 0
     
-    # E is a 3D array created by replicating Y - F across dimensions
-    
-    
-    # Convert the 2D matrix into a 3D array with dimensions (100, 1, 1000)
     Ex <- array((Y - F), dim = c(nrow(Y - F), 1, ncol(Y - F)))
     
     E <- aperm(array(rep(Ex, each = n), dim = c(dim(Ex)[1], n, dim(Ex)[3])), c(2, 1, 3))
@@ -199,15 +181,7 @@ validity <- function(y, X, reg, v = 0, s = 0,l=0,r=0) {
     
     print("Error:Test version not properly specified!")
   }
-  # Compute the mean along the first dimension (rows)
   
-  # Mean along the first dimension (rows)
-  # library(pracma)
-  # h <- repmat(matrix(1:n, n, 1), 1, n)
-  # t <- mean(apply(g * g / h, 2, mean))
-  # H <- array(rep(1:n, times = n * N), dim = c(n, n, N))
-  # T <- apply(G * G / H, c(2, 3), mean) 
-  # T <- apply(T, 2, mean)  
   
   
   if (t <= .Machine$double.eps){
@@ -215,30 +189,48 @@ validity <- function(y, X, reg, v = 0, s = 0,l=0,r=0) {
     p = 1
   }else{
     p <- 1 - sum(T <= t) / N}
-  #return(list(P-Value= p , t-Value = t))
   message1 <- "The validity test was successfully completed."
   message2 <- "H0: The model is considerd to be valid."
   message3 <- "H1: The model is not considered to be valid."
   
-  
-  # Ergebnisse anzeigen
   cat(message1, "\n")
   cat(message2, "\n")
   cat(message3, "\n")
   cat("t-Value:", t, "\n")
   cat("p-Value:", p, "\n")
   
-  # Optionale Rückgabe der Werte
   return(list(t_value = t, p_value = p))
 }
 
 reg<-function(y, X) {
+  #' The default OLS regression
+  #'
+  #' This function generates fitted values based on a set of regressors X and the independent variable y.
+  #' @param y Dependent variable, vector of (nx1)
+  #' @param X Regressors, can be either a vector (nx1) or a matrix (nxm) if there is more than one regressor
+  #' @return The function returns the fitted values based on the input and the use of standard OLS (including a constant)
+  #' @examples 
+  #' x <- rnorm(100, mean=0, sd=1)
+  #' y <- 10*x
+  #' reg(y,x)
+  #' @author Florian Schuetze 26.08.2024
   d<-data.frame(X,y1=y)
   model <- lm(y1 ~ ., data = d)
   return(model$fitted.values)
 }
 
 Example1 <- function(c,n) {
+  #' The first example of Frahm (2023)
+  #'
+  #' This function replicates the first example of Frahm (2023)
+  #' @param c A constant
+  #' @param n The number of observations
+  #' @return The function returns the p-value of the validity test.
+  #' @examples 
+  #' Example1(0.25,100)
+  #' @author Florian Schuetze 26.08.2024
+  #' @references 
+  #'Frahm, G., 2023, A Test for the Validity of Regression Models. Available on SSRN: https://ssrn.com/abstract=4610329
   x <- rnorm(n, mean=0, sd=1)
   y <- rnorm(n, mean=0, sd=1)
   y[which(x>0)]<- y[which(x>0)]+c
@@ -248,6 +240,17 @@ Example1 <- function(c,n) {
 }
 
 Example2 <- function(c,n) {
+  #' The second example of Frahm (2023)
+  #'
+  #' This function replicates the second example of Frahm (2023)
+  #' @param c A constant
+  #' @param n The number of observations
+  #' @return The function returns the p-value of the validity test.
+  #' @examples 
+  #' Example2(0.25,100)
+  #' @author Florian Schuetze 26.08.2024
+  #' @references 
+  #'Frahm, G., 2023, A Test for the Validity of Regression Models. Available on SSRN: https://ssrn.com/abstract=4610329
   x <- rnorm(n, mean=0, sd=1)
   y<-(-1)+x+c*((x^2)-1)+rnorm(n, mean=0, sd=1)
   p<-validity(x,y)
@@ -255,7 +258,17 @@ Example2 <- function(c,n) {
 }
 
 Example3 <- function(c,n) {
-  
+  #' The third example of Frahm (2023)
+  #'
+  #' This function replicates the third example of Frahm (2023)
+  #' @param c A constant
+  #' @param n The number of observations
+  #' @return The function returns the p-value of the validity test.
+  #' @examples 
+  #' Example3(0.25,100)
+  #' @author Florian Schuetze 26.08.2024
+  #' @references 
+  #'Frahm, G., 2023, A Test for the Validity of Regression Models. Available on SSRN: https://ssrn.com/abstract=4610329
   require(MASS)
   sigma<-rbind(c(1,0.5), c(0.5,1))
   mu<-c(0, 0) 
