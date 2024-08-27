1fac2e846b5a4a4a60b4be6b75dfa857c5c67cd8
Florian Schuetze <florian.schuetze@uni-hamburg.de>
2024-08-27 09:21:04 +0200

    asd

--- R/Validity.R
+++ R/Validity.R
@@ -2,7 +2,7 @@
 validity <- function(y, X, reg, v = 0, s = 0,l=0,r=0,N=1000,text=0) {
   #' Test for the Validity of Regression Models 
   #'
-  #' It tests if a regression is valid. A specific regression can be specified for this, otherwise OLS is the default option.
+  #' validity tests if a regression is valid. An own regression can be specified for this, otherwise OLS is the default option. The function returns the corresponding p-value and t-value.
   #' @param y Dependent variable, vector of (nx1)
   #' @param X Regressors, can be either a vector (nx1) or a matrix (nxm) if there is more than one regressor
   #' @param reg (optional, default is OLS) It is possible to define your own regression function. The output of the function must be the fitted values. The input must be the dependent variable and the independent variables. See the function "reg" in the readme file as an example.
